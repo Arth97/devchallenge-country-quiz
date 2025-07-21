457040f1a634a38007c4b7a7e3766f0660300ef5
Arturo <arturotinajas@gmail.com>
2025-07-21 21:10:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import Results from './pages/Results/results';
 import Quiz from './pages/Quiz/quiz';
 import React from 'react';
 
-const baseUrl = "/devchallenge-country-quiz"
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 		<BrowserRouter basename="/devchallenge-country-quiz">
 			<Routes>
-				<Route path="*" element={<Quiz />} />
 				<Route path="/" element={<Quiz />} />
 				<Route path=":questionIndex" element={<Quiz />} />
 				<Route path="results" element={<Results />} />
+				<Route path="*" element={<Quiz />} />
 			</Routes>
 		</BrowserRouter>
   </React.StrictMode>
